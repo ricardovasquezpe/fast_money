1ad28cd37a0c09672c1a689212259a70a75cf0f9
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2017-11-26 11:35:12 -0500

    filtering job

--- db/model/jobModel.js
+++ db/model/jobModel.js
@@ -147,7 +147,7 @@ module.exports = function(app, jwt){
     });
   });
 
-  app.post('/api/alljobs', function(req, res){
+  /*app.post('/api/alljobs', function(req, res){
     lastdate = (req.body.lastdate.length == 0) ? new Date() : req.body.lastdate;
 
     job.find({ created_at : {"$lt": lastdate} }, { '_id' : 1, 'title' : 1, 'description' : 1, 'payment' : 1, 'location' : 1, 'created_at' : 1 }).sort({created_at: -1}).limit(10).exec(function(err, jobs) { 
@@ -166,14 +166,15 @@ module.exports = function(app, jwt){
         );
       return;
      });
-  });
+  });*/
 
   app.post('/api/filterjobs', function(req, res){
+    lastdate = (req.body.lastdate.length == 0) ? new Date() : req.body.lastdate;
 
 
-    job.find({ title : { $regex: '.*' + req.body.description + '.*', $options: "i" }, "payment.type" : { $regex: '.*' + req.body.type + '.*'}, 
+    job.find({ created_at : {"$lt": lastdate} }, title : { $regex: '.*' + req.body.description + '.*', $options: "i" }, "payment.type" : { $regex: '.*' + req.body.type + '.*'}, 
                "location.country" : { $regex: '.*' + req.body.country + '.*', $options: "i"}/*,
-               "payment.amount" : { $gt: req.body.description, $lt: req.body.description }*/}, { '_id' : 1, 'title' : 1, 'description' : 1, 'payment' : 1, 'location' : 1, 'created_at' : 1 }).sort({created_at: -1}).exec(function(err, jobs) { 
+               "payment.amount" : { $gt: req.body.description, $lt: req.body.description }*/}, { '_id' : 1, 'title' : 1, 'description' : 1, 'payment' : 1, 'location' : 1, 'created_at' : 1 }).sort({created_at: -1}).limit(10).exec(function(err, jobs) { 
 
         if (!jobs){
             res.json(
