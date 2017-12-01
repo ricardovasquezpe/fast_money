de200617545ee196f48e01fb0c171d269b34393e
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2017-12-01 08:36:20 -0500

    Adding bid and favorite

--- db/model/favoriteModel.js
+++ db/model/favoriteModel.js
@@ -35,6 +35,36 @@ module.exports = function(app, jwt){
 
   });
 
+  app.post('/api/removefavorite', function(req, res){
+    req.check('id_favorite', 'Invalid id_favorite').notEmpty();
+
+    var error = req.validationErrors();
+    if(error){
+      res.json(
+        {"status" : false,
+         "data"   : error}
+      );
+      return;
+    }
+
+    favorite.remove({ _id: req.body.id_favorite }, function(err) {
+        if (err) {
+            res.json(
+              {"status" : false,
+               "data"   : "Weird error"}
+            );
+            return;
+        }
+        res.json(
+          {"status" : true,
+           "data"   : 'Favorite removed!'}
+        );
+        return;
+    });
+
+  });
+
+
   app.post('/api/myfavorites', function(req, res){
   	favorite.find({ id_user : req.decoded._doc._id }, { '_id' : 1, 'title' : 1, 'location' : 1 }, function(err, jobs) {
         if (!jobs){
